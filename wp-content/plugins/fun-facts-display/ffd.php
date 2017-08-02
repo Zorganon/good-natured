e9f52e6859a2e180555192a2b2cba172e6cd6e3c
Dan <dan.the.hudson@gmail.com>
2017-08-02 19:17:50 +0000

    added query to db for funfact posts

--- wp-content/plugins/fun-facts-display/ffd.php
+++ wp-content/plugins/fun-facts-display/ffd.php
@@ -11,9 +11,18 @@ Author: Matt Mullenweg
 Version: 1.6
 Author URI: http://ma.tt/
 */
+  
+global $wpdb;
+
 function get_fun_facts() {
  //retrieves, cleans and returns a random funfact post
-  
+ // get_results returns a numerically indexed array of row objects
+  $results = $wpdb->get_results( 'SELECT * FROM wp_posts WHERE post_type = funfact', OBJECT );
+  // choose a random member of results array
+  $randomfunfactkey = array_rand($results, 1);
+  $funfactObject = $results($randomfunfactkey);
+  $funfactString = apply_filters( 'the_content', $funfactObject->post_content );
+  return $funfactString;
 }
 
 function show_fun_fact() {
@@ -22,9 +31,9 @@ function show_fun_fact() {
   
 }
 
-add_action( 'wp_load', 'show_fun_fact' );
+add_action( 'wp_head', 'show_fun_fact' );
 
 function fun_fact_css() {
   //load funfact css file.
 }
-add_action( 'wp_load', 'fun_fact_css');
+add_action( 'wp_enqueue_scripts', 'fun_fact_css');
