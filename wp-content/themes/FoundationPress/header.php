c92ab9be70c179766458143057744a15058a9fc7
Dan <dan.the.hudson@gmail.com>
2017-08-10 20:30:54 +0000

    updating styles

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -16,7 +16,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
 	</head>
-	<body <?php body_class(); ?>>
+	<body class="container">
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
@@ -27,19 +27,29 @@
 
 	<header class="site-header" role="banner">
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
+			<!--
 			<div class="title-bar-left">
 				<button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 				<span class="site-mobile-title title-bar-title">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</span>
 			</div>
+			-->
 		</div>
+		<p> DOES THIS EVEN SHOWUP?</p>
 
-		<nav class="site-navigation top-bar" role="navigation">
-			<div class="top-bar-left">
-				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</div>
+		<nav class="site-navigation top-bar nav-bar" role="navigation">
+			<div class="site-navigation__bar">
+				<a class="site-navigation__button button primary" href="#">HOME</a>
+				<a class="site-navigation__button button primary" href="#">ABOUT</a>
+				<a class="site-navigation__button button primary" href="#">RECIPES</a>
+				<a class="site-navigation__button button primary" href="#">LEARN</a>
+				<a class="site-navigation__button button primary" href="#">NEWS</a>
+				<a class="site-navigation__button button primary" href="#">CONTACT</a>
+			</div>
+				
+			<div class="site-desktop-title top-bar-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			</div>
 			<div class="top-bar-right">
 				<?php foundationpress_top_bar_r(); ?>
@@ -52,5 +62,5 @@
 		
 	</header>
 
-	<section class="container">
+	<section>
 		<?php do_action( 'foundationpress_after_header' );
