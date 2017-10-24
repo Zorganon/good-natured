bfbcd9e486462bf6f66f2d4a5dbd02e3aed1131a
Dan <dan.the.hudson@gmail.com>
2017-10-24 03:48:03 +0000

    added basic recipe post template

---
+++ wp-content/themes/FoundationPress/recipe_template.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Recipe post
+*/
+get_header(); ?>
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+<div class="page-container">
+	<div class="body-content-container">
+	  <div class="r-seeall-button"></div>
+	  <div class="recipe-post-blurb-container">
+	    <div class="recipe-post__title"><?php insert title ?></div>
+	    <div class="recipe-post__tags-list"><?php insert tags ?></div>
+	    <div class="recipe-post-blurb__row row">
+	      <div class="recipe-post-blurb__image column small-6"></div>
+	      <div class="recipe-post-blurb__text-box column small-6">
+	        <h3>Did you know?</h3>
+	        <p><?php get factoid field content ?></p>
+	      </div>
+	    </div>
+	    <div class="recipe-post-main row">
+	      <div class="recipe-post-main__ingredients">
+	        <?php insert ingredients into list ?>
+	      </div>
+	      <div class="recipe-post-main__instructions">
+	        <?php insert instructions ?>
+	      </div>
+	    </div>
+	  </div>
+	  
+  </div>
+</div>
