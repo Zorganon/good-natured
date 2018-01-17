f36f9f73615e1a3bbf481352444324decf60408f
Dan <dan.the.hudson@gmail.com>
2018-01-17 00:10:02 +0000

    converted navbar to bootstrap

--- wp-content/themes/astra-child/header.php
+++ wp-content/themes/astra-child/header.php
@@ -53,7 +53,7 @@
 			</div>
 		</div>
 	</header>
-	<nav class="nav-container nav-bar" role="navigation">
+	<nav class="nav-container navbar" role="navigation">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -62,55 +62,16 @@
     </div>
 	  <div class="container-fluid">
 			<div class="site-navigation__bar flex-container">
-				<a type="button" class="button large site-navigation__button" href="/home">HOME</a>
-				<a type="button" class="button large site-navigation__button" href="/about">ABOUT</a>
-				<a type="button" class="button large site-navigation__button" href="/recipes">RECIPES</a>
-				<a type="button" class="button large site-navigation__button" href="/news">NEWS</a>
-				<a type="button" class="button large site-navigation__button" href="/contact">CONTACT</a>
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+						<li><a type="button" class="button large site-navigation__button" href="/home">HOME</a></li>
+						<li><a type="button" class="button large site-navigation__button" href="/about">ABOUT</a></li>
+						<li><a type="button" class="button large site-navigation__button" href="/recipes">RECIPES</a></li>
+						<li><a type="button" class="button large site-navigation__button" href="/news">NEWS</a></li>
+						<li><a type="button" class="button large site-navigation__button" href="/contact">CONTACT</a></li>
+					</ul>
+				</div>	
 			</div>
 		</div>
 	</nav>
-	<nav class="navbar navbar-default">
-    <!-- Brand and toggle get grouped for better mobile display -->
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>	
 
