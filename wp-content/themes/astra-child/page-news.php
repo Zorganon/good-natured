e550b05e361324f0a224e8fc8dfdaaa4ad4a7efb
Dan <dan.the.hudson@gmail.com>
2018-01-19 22:21:51 +0000

    added bootstrap columns to post grid pages

--- wp-content/themes/astra-child/page-news.php
+++ wp-content/themes/astra-child/page-news.php
@@ -4,38 +4,37 @@ Template Name: News
 */
 get_header(); ?>
 
-<div class="page-container">
-	<div class="body-content-container">
-		
-		<div class="post-page-grid">
-			<div class="green-dashed-divider"></div>
-			<div class="post-page-grid-content">
+<div class="body-content-container">
+	
+	<div class="post-page-grid">
+		<div class="green-dashed-divider"></div>
+		<div class="post-page-grid-content container">
+			<div class="row">
 				<?php $args = array('post_type'=>'post');
 			
 			    $nloop = new WP_Query($args);
 			
 			    while ($nloop->have_posts()) : $nloop->the_post(); 
 				?>
-			    <div class="post-tile__tile col-sm-12 col-md-4">
-				    <a href="<?php echo get_permalink(); ?>">
-				      <div class="post-tile__img">
-				        <?php echo the_post_thumbnail( 'medium-large' ); ?>
-				      </div>
-				      <div class="post-tile__title">
-				          <?php echo the_title(); ?>
-				      </div>
-				      <div class="blue-line"></div>
-				    </a>
-			    </div>
+		    <div class="post-tile__tile col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+			    <a href="<?php echo get_permalink(); ?>">
+			      <div class="post-tile__img">
+			        <?php echo the_post_thumbnail( 'medium-large' ); ?>
+			      </div>
+			      <div class="post-tile__title">
+			          <?php echo the_title(); ?>
+			      </div>
+			      <div class="blue-line"></div>
+			    </a>
+		    </div>
 			  <?php 
 			    endwhile;
 			    wp_reset_postdata();
-			    ?>
-			</div>
+		    ?>
+	  	</div>
 		</div>
-		
 	</div>
 	
-	
 </div>
+	
 <?php get_footer();
--- wp-content/themes/astra-child/page-recipes.php
+++ wp-content/themes/astra-child/page-recipes.php
@@ -4,19 +4,19 @@ Template Name: recipe
 */
 get_header(); ?>
 
-<div class="page-container">
-	<div class="body-content-container">
-		
-		<div class="post-page-grid">
-			<div class="green-dashed-divider"></div>
-			<div class="post-page-grid-content">
+<div class="body-content-container">
+	
+	<div class="post-page-grid">
+		<div class="green-dashed-divider"></div>
+		<div class="post-page-grid-content container">
+			<div class="row">
 				<?php $args = array('post_type'=>'recipe');
 			
 			    $rloop = new WP_Query($args);
 			
 			    while ($rloop->have_posts()) : $rloop->the_post(); 
 				?>
-			    <div class="post-tile__tile col-sm-12 col-md-4">
+			    <div class="post-tile__tile col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
 			    	<a href="<?php echo get_permalink(); ?>">	
 				      <div class="post-tile__img">
 				        <?php echo the_post_thumbnail( 'medium-large' ); ?>
@@ -27,18 +27,18 @@ get_header(); ?>
 				      <div class="blue-line"></div>
 			      </a>
 			    </div>
-			<?php 
+				<?php 
 		    endwhile;
 		    wp_reset_postdata();
 		    ?>
 			</div>
 		</div>
-		
-
-		<?php do_action( 'foundationpress_after_content' ); ?>
-		
 	</div>
 	
+
+	<?php do_action( 'foundationpress_after_content' ); ?>
 	
 </div>
+	
+	
 <?php get_footer();
