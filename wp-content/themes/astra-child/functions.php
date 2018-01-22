e5e0d3907ff84eb2ceb32faa41267f10cc7360a1
Dan <dan.the.hudson@gmail.com>
2018-01-22 22:21:12 +0000

    renamed about page styles to correct loading issue

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -9,7 +9,7 @@ function real_upload_url() {
 function gnn_enqueue_styles() {
   wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.css');
   wp_enqueue_style( 'front', get_stylesheet_directory_uri() . '/assets/css/front.css');
-  wp_enqueue_style( 'about', get_stylesheet_directory_uri() . '/assets/css/about.css');
+  wp_enqueue_style( 'aboutpage', get_stylesheet_directory_uri() . '/assets/css/about.css');
   wp_enqueue_style( 'news', get_stylesheet_directory_uri() . '/assets/css/news.css');
   wp_enqueue_style( 'recipes', get_stylesheet_directory_uri() . '/assets/css/recipes.css');
 }
